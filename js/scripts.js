5b844ebed8fb1aaeff6b2584605ab0daf681847d
Student <example@example.com>
2019-07-22 19:48:20 -0700

    made funcitoning form

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
     
     
-    var commitment = $("time-commitment").val();
-
-    $("#workout-form").submit(function(event){
-        event.preventdefault;
-        if (commmitment === 5) {
-            $(".5dayresult").show();
-        } if else (commitment === 4) {
-            $(".4dayresult").show();
-        } else (commitment === 3) {
-            $(".3dayresult").show
+    
+    $("form#workout-form").submit(function(event){
+        var commitment = $("#time-commitment").val();
+        debugger;
+        $("form#workout-form").hide();
+        if (commitment == 5) {
+            $("#fivedayresults").show();
+        } else if (commitment == 4) {
+            $("#fourdayresults").show();
+        } else {
+            $("#threedayresults").show();
         };
+        event.preventDefault();
     });
 });
